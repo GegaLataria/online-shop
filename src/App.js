6c57ac5878b9bed9a39050fe711a28622e1d9a8f
Gega <latariagega1@gmail.com>
2022-09-12 18:26:49 +0400

    implementing slider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import Header from "./header/Header";
+import ImageSlider from "./slider/ImageSlider";
 
 const App = () => {
+  const slides = [
+    { url: "http://localhost:3000/image-1.jpg", title: "Beach" },
+    { url: "http://localhost:3000/image-2.jpg", title: "Boat" },
+    { url: "http://localhost:3000/image-3.jpg", title: "Forest" },
+    { url: "http://localhost:3000/image-4.jpg", title: "City" },
+    { url: "http://localhost:3000/image-5.jpg", title: "Italy" },
+  ];
+
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+  };
   return (
     <div>
       <Header />
+      <div style={containerStyles}>
+        <ImageSlider slides={slides} />
+      </div>
     </div>
   );
 };
