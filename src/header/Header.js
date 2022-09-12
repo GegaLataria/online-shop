e402efb4c927738be6166c8f16877a5790e8b781
Gega <latariagega1@gmail.com>
2022-09-12 13:29:29 +0400

    implementing header

---
+++ src/header/Header.js
@@ -0,0 +1,26 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-list">
+        <a href="https://extra.ge/" className="header-list__item">
+          Home
+        </a>
+        <a href="https://extra.ge/" className="header-list__item">
+          Home
+        </a>
+        <a href="https://extra.ge/" className="header-list__item">
+          Home
+        </a>
+        <a href="https://extra.ge/" className="header-list__item">
+          Home
+        </a>
+        <a href="https://extra.ge/" className="header-list__item">
+          Home
+        </a>
+      </div>
+    </div>
+  );
+};
+export default Header;
