88a23833e1855f75dd0fb1cd788b68abae3d9826
Gega <latariagega1@gmail.com>
2022-09-14 20:01:57 +0400

    updated footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,21 +1,21 @@
+import "./Footer.css";
+
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-list">
-        <div className="footer-list__item">
-          <a href="https://extra.ge/" className="footer-list__item">
-            წესები და პირობები
-          </a>
-          <a href="https://extra.ge/" className="footer-list__item">
-            კონტაქტი
-          </a>
-          <a href="https://extra.ge/" className="footer-list__item">
-            პარტნიორები
-          </a>
-          <a href="https://extra.ge/" className="footer-list__item">
-            ჩვენ შესახებ
-          </a>
-        </div>
+        <a href="https://extra.ge/" className="footer-list__item">
+          წესები და პირობები
+        </a>
+        <a href="https://extra.ge/" className="footer-list__item">
+          კონტაქტი
+        </a>
+        <a href="https://extra.ge/" className="footer-list__item">
+          პარტნიორები
+        </a>
+        <a href="https://extra.ge/" className="footer-list__item">
+          ჩვენ შესახებ
+        </a>
       </div>
     </div>
   );
