74b55304631eaeb26ad1d46e881536ccd991cb69
Gega <latariagega1@gmail.com>
2022-09-14 16:31:36 +0400

    updating landing page

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,12 +1,41 @@
+import { useRef } from "react";
 import ImageSlider from "../slider/ImageSlider";
 import "./Home.css";
 
 const Home = ({ slides }) => {
+  const ref = useRef(null);
+  const handleClick = () => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <div>
       <ImageSlider slides={slides} />
       <div className="home-container">
-        <button className="home-shop-button">იშოპინგე</button>
+        <button className="home-shop-button" onClick={handleClick}>
+          იშოპინგე
+        </button>
+      </div>
+      <div ref={ref} className="products-section">
+        <h2>პოპულარული პროდუქცია</h2>
+        <div className="products-section__list">
+          <div className="products-section__list__item">
+            <a href="www.sleek.ge">უთო</a>
+          </div>
+          <div className="products-section__list__item">
+            <a href="www.sleek.ge">გაზქურა</a>
+          </div>
+          <div className="products-section__list__item">
+            <a href="www.sleek.ge">ფენი</a>
+          </div>
+          <div className="products-section__list__item">
+            <a
+              className="products-section__list__item__input"
+              href="www.sleek.ge"
+            >
+              ტელეფონი
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
