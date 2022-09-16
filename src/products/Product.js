2e7df7bf539df09acc37a786881caa06b8682fac
Gega <latariagega1@gmail.com>
2022-09-16 18:31:51 +0400

    updating product page

--- src/products/Product.js
+++ src/products/Product.js
@@ -1,7 +1,10 @@
+import iphone from "./iphone.jpg";
+import "./Product.css";
 const Product = () => {
   return (
-    <div>
+    <div className="product-page">
       <h2 className="product-header">Iphone 14 Pro Max</h2>
+      <img src={iphone} alt="iphone-photo" className="product-image"></img>
     </div>
   );
 };
