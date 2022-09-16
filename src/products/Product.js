5e8875090baf998d91a3d09076fee20b0a8a6303
Gega <latariagega1@gmail.com>
2022-09-16 19:16:24 +0400

    added product description

--- src/products/Product.js
+++ src/products/Product.js
@@ -4,7 +4,27 @@ const Product = () => {
   return (
     <div className="product-page">
       <h2 className="product-header">Iphone 14 Pro Max</h2>
-      <img src={iphone} alt="iphone-photo" className="product-image"></img>
+
+      <div className="product-page__container">
+        <div className="product-page__container__column">
+          <img src={iphone} alt="iphone-photo" className="product-image"></img>
+        </div>
+        <div className="product-page__container__column">
+          <h3>მოკლე აღწერა:</h3>
+          <p>ეკრანის ზომა: 6.7"</p>
+          <p>შიდა მეხსიერება: 256GB</p>
+          <p>ოპერატიული მეხსიერება: 6 GB</p>
+          <p>მთავარი კამერა: 12+12+12 MP</p>
+          <p>• უფასო მიწოდება 50 ლარიდან</p>
+          <p>• მიწოდება თბილისში 1-3 სამუშაო დღე</p>
+          <p>• მიწოდება საქართველოს მასშტაბით 3-5 სამუშაო დღე</p>
+          <p>• გადახდა ნებისმიერი ბანკის ბარათით ან განვადებით</p>
+          <p>• დაბრუნება შესაძლებელია მიღებიდან 14 დღის განმავლობაში</p>
+        </div>
+      </div>
+      <h2>ფასი: 4199₾</h2>
+      <button>ყიდვა</button>
+      <button>კალათაში დამატება</button>
     </div>
   );
 };
