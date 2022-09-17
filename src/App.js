29c55bc52429830ca6643b2a31588c6c1e41cd5c
Gega <latariagega1@gmail.com>
2022-09-17 11:54:44 +0400

    updated slider

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Header from "./header/Header";
 
 const App = () => {
   const slides = [
-    { url: "http://localhost:3000/image-1.jpg", title: "Beach" },
-    { url: "http://localhost:3000/image-2.jpg", title: "Boat" },
-    { url: "http://localhost:3000/image-3.jpg", title: "Forest" },
+    { url: "http://localhost:3000/image-1.jpg", title: "Iphone" },
+    { url: "http://localhost:3000/image-2.jpg", title: "Watch" },
+    { url: "http://localhost:3000/image-3.jpg", title: "Macbook" },
     { url: "http://localhost:3000/image-4.jpg", title: "City" },
     { url: "http://localhost:3000/image-5.jpg", title: "Italy" },
   ];
