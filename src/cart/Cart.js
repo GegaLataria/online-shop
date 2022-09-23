b58435c35f4c8fadf22be74c982ed3dc30599b7f
Gega <latariagega1@gmail.com>
2022-09-23 18:39:25 +0400

    fixed quantities in cart

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -1,36 +1,16 @@
 import { Context } from "../App";
 import React from "react";
 import "./Cart.css";
-import exportedObject from "../products/CartButton";
+// import exportedObject from "../products/CartButton";
+import CartItems from "./CartItems";
 
 const Cart = () => {
   const value = React.useContext(Context);
 
-  const handleClick = () => {
-    value.setCartItems(0);
-    value.setProducts([]);
-    window.localStorage.setItem("cartItems", JSON.stringify(0));
-    window.localStorage.setItem("product", JSON.stringify([]));
-  };
-
-  const handleSubtract = () => {
-    if (value.cartItems > 1) {
-      value.setCartItems(value.cartItems - 1);
-      window.localStorage.setItem(
-        "cartItems",
-        JSON.stringify(value.cartItems - 1)
-      );
-      window.localStorage.setItem(
-        "product",
-        JSON.stringify({ header: header, image: image, price: price })
-      );
-    }
-  };
-
   let product = JSON.parse(localStorage.getItem("product"));
   console.log("cart product", product);
   if (product) {
-    product = product[1];
+    product = product[0];
   }
 
   const { header, image, price, index } = product || {};
@@ -43,54 +23,13 @@ const Cart = () => {
           ? `თქვენ გაქვთ ${value.cartItems} ნივთი კალათაში`
           : `თქვენი კალათა ცარიელია`}
       </h2>
-      {header ? (
-        <div className="cart-page-container">
-          <div className="cart-page-container__item">
-            <img src={image} alt="product" className="cart-image"></img>
-          </div>
-          <div className="cart-page-container__item">
-            <div className="cart-numbers-button">
-              <h2
-                onClick={handleSubtract}
-                className="cart-numbers-button__item"
-              >
-                —
-              </h2>
-              <h2>{value.cartItems}</h2>
-              <h2
-                onClick={() =>
-                  exportedObject.handleClick(
-                    { header, image, price, index },
-                    value
-                  )
-                }
-                className="cart-numbers-button__item"
-              >
-                +
-              </h2>
-            </div>
-          </div>
-          <div className="cart-page-container__item">
-            <h2>{header}</h2>
-            <h2>{price}₾</h2>
-          </div>
-          <div className="cart-page-container__item">
-            <h2>სულ თანხა: </h2>
-            <h2>{value.cartItems * price}₾</h2>
-          </div>
-          <div className="cart-page-container__item">
-            <button onClick={handleClick} className="clear-cart-button">
-              წაშლა
-            </button>
-          </div>
-        </div>
-      ) : null}
-      <br></br>
-      {header ? (
-        <button className="clear-cart-button" onClick={handleClick}>
-          კალათის გასუფთავება
-        </button>
-      ) : null}
+      <CartItems
+        header={header}
+        price={price}
+        index={index}
+        image={image}
+        value={value}
+      />
     </div>
   );
 };
---
+++ src/cart/CartItems.js
@@ -0,0 +1,82 @@
+import React from "react";
+import exportedObject from "../products/CartButton";
+
+export default function CartItems({ header, image, price, index, value }) {
+  const handleClick = () => {
+    value.setCartItems(0);
+    value.setProducts([]);
+    window.localStorage.setItem("cartItems", JSON.stringify(0));
+    window.localStorage.setItem("product", JSON.stringify([]));
+  };
+
+  const handleSubtract = () => {
+    const exist = value.products.find((x) => x.id === index);
+
+    if (value.cartItems > 1) {
+      value.setCartItems(value.cartItems - 1);
+      window.localStorage.setItem(
+        "cartItems",
+        JSON.stringify(value.cartItems - 1)
+      );
+      value.setProducts(
+        value.products.map((x) =>
+          x.id === index ? { ...exist, amount: exist.amount - 1 } : x
+        )
+      );
+      window.localStorage.setItem("product", JSON.stringify(value.products));
+    }
+  };
+
+  return (
+    <div>
+      {header ? (
+        <div className="cart-page-container">
+          <div className="cart-page-container__item">
+            <img src={image} alt="product" className="cart-image"></img>
+          </div>
+          <div className="cart-page-container__item">
+            <div className="cart-numbers-button">
+              <h2
+                onClick={handleSubtract}
+                className="cart-numbers-button__item"
+              >
+                —
+              </h2>
+              <h2>{value.cartItems}</h2>
+              <h2
+                onClick={() =>
+                  exportedObject.handleClick(
+                    { header, image, price, index },
+                    value
+                  )
+                }
+                className="cart-numbers-button__item"
+              >
+                +
+              </h2>
+            </div>
+          </div>
+          <div className="cart-page-container__item">
+            <h2>{header}</h2>
+            <h2>{price}₾</h2>
+          </div>
+          <div className="cart-page-container__item">
+            <h2>სულ თანხა: </h2>
+            <h2>{value.cartItems * price}₾</h2>
+          </div>
+          <div className="cart-page-container__item">
+            <button onClick={handleClick} className="clear-cart-button">
+              წაშლა
+            </button>
+          </div>
+        </div>
+      ) : null}
+      <br></br>
+      {header ? (
+        <button className="clear-cart-button" onClick={handleClick}>
+          კალათის გასუფთავება
+        </button>
+      ) : null}
+    </div>
+  );
+}
