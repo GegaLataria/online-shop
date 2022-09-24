fd584553f6d1cf71cf1f4a506151832a02f1a6e2
Gega <latariagega1@gmail.com>
2022-09-24 13:15:44 +0400

    implemented: cart does not show number if it is empty

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
         <a href="https://extra.ge/" className="header-list__item">
           სილამაზე
         </a>
-        <a href="/cart" className="header-list__item header-list__cart">🛒<span className="cart-items">{value.cartItems}</span></a>
+        <a href="/cart" className="header-list__item header-list__cart">🛒{value.cartItems?<span className="cart-items">{value.cartItems}</span>:null}</a>
       </div>
     </div>
   );
