1f4a49436a0fff500968ea09fe1a78b8ab41e02a
Gega <latariagega1@gmail.com>
2022-10-07 14:45:19 +0400

    implementing order confirmation on click

--- src/orderDetails/OrderDetails.js
+++ src/orderDetails/OrderDetails.js
@@ -1,51 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./OrderDetails.css";
 
 export default function OrderDetails() {
+  const [buyText, setBuyText] = useState(null);
+
+  const handleClick = () => {
+    setBuyText("Order is Confirmed!");
+  };
+
   return (
     <div>
-      <h2 className="cart-header">Buy</h2>
-      <div className="form-container">
-        <form className="form-container__list">
-          <input
-            className="form-container__list__item"
-            type={"text"}
-            placeholder="First Name"
-            required
-          ></input>
-          <input
-            className="form-container__list__item"
-            type={"text"}
-            placeholder="Last Name"
-            required
-          ></input>
-          <input
-            className="form-container__list__item"
-            type={"email"}
-            placeholder="Email"
-            required
-          ></input>
-          <input
-            className="form-container__list__item"
-            type={"text"}
-            placeholder="Country"
-            required
-          ></input>
-          <input
-            className="form-container__list__item"
-            type={"text"}
-            placeholder="City"
-            required
-          ></input>
-          <input
-            className="form-container__list__item"
-            type={"text"}
-            placeholder="Address"
-            required
-          ></input>
-          <button className="clear-cart-button">Submit</button>
-        </form>
-      </div>
+      {!buyText ? (
+        <div>
+          <h2 className="cart-header">Buy</h2>
+          <div className="form-container">
+            <form className="form-container__list">
+              <input
+                className="form-container__list__item"
+                type={"text"}
+                placeholder="First Name"
+                required
+              ></input>
+              <input
+                className="form-container__list__item"
+                type={"text"}
+                placeholder="Last Name"
+                required
+              ></input>
+              <input
+                className="form-container__list__item"
+                type={"email"}
+                placeholder="Email"
+                required
+              ></input>
+              <input
+                className="form-container__list__item"
+                type={"text"}
+                placeholder="Country"
+                required
+              ></input>
+              <input
+                className="form-container__list__item"
+                type={"text"}
+                placeholder="City"
+                required
+              ></input>
+              <input
+                className="form-container__list__item"
+                type={"text"}
+                placeholder="Address"
+                required
+              ></input>
+              <button className="clear-cart-button" onClick={handleClick}>
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      ) : (
+        <h1>{buyText}</h1>
+      )}
     </div>
   );
 }
