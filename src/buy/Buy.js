0c4770fff318a1cf0304f081f63c4121ed88b62e
Gega <latariagega1@gmail.com>
2022-10-07 14:22:05 +0400

    added inputs for order details page

--- src/buy/Buy.js
+++ src/buy/Buy.js
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function Buy() {
-  return <div>buy</div>;
+  return (
+    <div>
+      <h2 className="cart-header">Buy</h2>
+      <div className="form-container">
+        <form>
+          <input type={"text"} placeholder="First Name" required></input>
+          <input type={"text"} placeholder="Last Name" required></input>
+          <input type={"email"} placeholder="Email" required></input>
+          <input type={"text"} placeholder="Country" required></input>
+          <input type={"text"} placeholder="City" required></input>
+          <input type={"text"} placeholder="Address" required></input>
+          <input type={"submit"} placeholder="Submit"></input>
+        </form>
+      </div>
+    </div>
+  );
 }
